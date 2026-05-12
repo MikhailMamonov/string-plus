06b45c4196d1f2816e886abd9140a2e8fe366ad4
Vanesa Karina <vaneskar@mu-l5.yar.21-school.ru>
2026-05-12 16:45:49 +0300

    [develop-4] library header

---
+++ src/s21_string.h
@@ -0,0 +1,12 @@
+#ifndef S21_STRING_H
+#define S21_STRING_H
+
+#include "types.h"
+void *s21_memchr(const void *str, int c, s21_size_t n);
+void *s21_memset(void *str, int c, s21_size_t n);
+int s21_strncmp(const char *str1, const char *str2, s21_size_t n);
+char *s21_strerror(int errnum);
+char *strrchr(const char *str, int c);
+
+#endif
+
