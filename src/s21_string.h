b0b3785215de76aa76300a17f10496b44df96e94
Britt Dennis <brittden@mu-j3.yar.21-school.ru>
2026-05-13 14:45:05 +0300

    [feature/memcpy-2] - added func into header file.

--- src/s21_string.h
+++ src/s21_string.h
@@ -3,6 +3,7 @@
 
 #include "types.h"
 void *s21_memchr(const void *str, int c, s21_size_t n);
+void *s21_memcpy(void *dest, const void *src, s21_size_t n);
 void *s21_memset(void *str, int c, s21_size_t n);
 int s21_strncmp(const char *str1, const char *str2, s21_size_t n);
 char *s21_strerror(int errnum);
