542a475087c39ea986af37d7fdb12d174fbb0385
Vanesa Karina <vaneskar@mu-l5.yar.21-school.ru>
2026-05-13 15:25:12 +0300

    [feature/memchr-4] Fixed loop test, variable names, makefile, gcov report

--- src/tests/test_memchr.c
+++ src/tests/test_memchr.c
@@ -2,14 +2,14 @@
 #include "../s21_string.h"
 #include <string.h>
 
-char *inputs_s[] = {"apple", "verter", "a", "supercalifragilisticexpialidocious", "Several words and 221 122 32"};
-char inputs_c[] = {'p', 'a', 'f', '\0', '1'};
-s21_size_t inputs_n[] = {3, 10, -1, 5, 14};
+char *memchr_inputs_s[] = {"apple", "verter", "a", "supercalifragilisticexpialidocious", "Several words and 221 122 32"};
+char memchr_inputs_c[] = {'p', 'a', 'f', '\0', ' '};
+s21_size_t memchr_inputs_n[] = {3, 5, 0, 5, 14};
 
 START_TEST(test_find) {
     // _i — это встроенная переменная индекса
-    char *res = s21_memchr(inputs_s[_i], inputs_c[_i], inputs_n[_i]);
-    char *expected = memchr(inputs_s[_i], inputs_c[_i], inputs_n[_i]);
+    char *res = s21_memchr(memchr_inputs_s[_i], memchr_inputs_c[_i], memchr_inputs_n[_i]);
+    char *expected = memchr(memchr_inputs_s[_i], memchr_inputs_c[_i], memchr_inputs_n[_i]);
     ck_assert_msg(res == expected,
         "Ошибка на итерации %d: ожидалось %p, получили %p",
         _i, expected, res);
@@ -20,7 +20,7 @@ END_TEST
 Suite *memchr_suite_create(void) {
     Suite *s = suite_create("Memchr");
     TCase *tc = tcase_create("Core");
-    tcase_add_test(tc, test_find);
+    tcase_add_loop_test(tc, test_find, 0, 5);
     suite_add_tcase(s, tc);
     return s;
 }
