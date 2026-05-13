391e27443076f2d4e7d591da2327250d7c31e156
Vanesa Karina <vaneskar@mu-l5.yar.21-school.ru>
2026-05-13 18:31:26 +0300

    [vaneskar:feature/memchr-6] Resolved issues

--- src/tests/test_main.c
+++ src/tests/test_main.c
@@ -1,13 +1,18 @@
 #include <check.h>
 #include <stdlib.h>
+#include "../s21_string.h"
 
 Suite *memchr_suite_create(void);
 Suite *memset_suite_create(void);
 
+/*void srunner_assembler(SRunner *sr) {
+    srunner_add_suite(sr, memset_suite_create());
+}*/
+
 int main(void) {
     int failed = 0;
     SRunner *sr = srunner_create(memchr_suite_create());
-    srunner_add_suite(sr, memset_suite_create());
+//  srunner_assembler(sr);
 
     srunner_run_all(sr, CK_NORMAL);
     failed = srunner_ntests_failed(sr);
