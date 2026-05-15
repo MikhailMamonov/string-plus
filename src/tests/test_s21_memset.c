4237f6868df36fda88087a55af568a1173603412
Vanesa Karina <vaneskar@us-n5.yar.21-school.ru>
2026-05-15 14:09:17 +0300

    [vaneskar:feature/memset-2] Resolved issues

--- src/tests/test_s21_memset.c
+++ src/tests/test_s21_memset.c
@@ -3,64 +3,71 @@
 #include "../s21_string.h"
 #include <string.h>
 
-char *memset_correct_inputs_s[] = {"apple", "verter", "alll", "supercalifragilisticexpialidocious", "Several words and 221 122 32"};
-int memset_correct_inputs_c[] = {'p', 'a', 'l', '\0', '1'};
-s21_size_t memset_correct_inputs_n[] = {3, 4, 1, 34, 14};
+#define CORRECT_CASES_NUM 5
+#define EDGE_CASES_NUM 3
+
+typedef struct {
+    char *string;
+    int chr;
+    s21_size_t num;
+} memsetParams;
+
+static const memsetParams* get_correct_memset_case(int index) {
+    // Массив изолирован внутри функции и невидим снаружи
+    static const memsetParams cases[] = {
+        {"apple", 'p', 3},
+        {"verter", 'a', 4},
+        {"alll", 'l', 1},
+        {"supercalifragilisticexpialidocious", '\0', 34},
+        {"Several words and 221 122 32", '1', 14}
+    };
+    return &cases[index];
+}
+
+static const memsetParams* get_edge_memset_case(int index) {
+    // Массив изолирован внутри функции и невидим снаружи
+    static const memsetParams cases[] = {
+        {"apple", -1, 3},
+        {"verter", 257, 4},
+        {"alll", 'l', 0}
+    };
+    return &cases[index];
+}
+
+char* create_buffer(s21_size_t len, char* str) {
+    char *buffer = malloc(len);
+    strcpy(buffer, str);
+    return buffer;
+}
+
+void test_memset_function(memsetParams current_case, char **res, char **expected, s21_size_t *len) {
+    *len = strlen(current_case.string) + 1;
+    char *buf_s21 = create_buffer(*len, current_case.string);
+    char *buf_orig = create_buffer(*len, current_case.string);
+
+    *res = s21_memset(buf_s21, current_case.chr, current_case.num);
+    *expected = memset(buf_orig, current_case.chr, current_case.num);
+}
 
 START_TEST(test_memset_correct_input_orig_compare) {
-    // 1. Создаем два изменяемых буфера в стеке под текущую строку
-    // Выделяем память с запасом (+1 для '\0')
-    s21_size_t len = strlen(memset_correct_inputs_s[_i]) + 1;
-    char *buf_s21 = malloc(len);
-    char *buf_orig = malloc(len);
+    const memsetParams *current_case = get_correct_memset_case(_i);
+    char *res = s21_NULL;
+    char *expected = s21_NULL;
+    s21_size_t len;
+    test_memset_function(*current_case, &res, &expected, &len);
 
-    // Копируем неизменяемый литерал в наши изменяемые буферы
-    strcpy(buf_s21, memset_correct_inputs_s[_i]);
-    strcpy(buf_orig, memset_correct_inputs_s[_i]);
-
-    // 2. Вызываем функции на изменяемой памяти
-    char *res = s21_memset(buf_s21, memset_correct_inputs_c[_i], memset_correct_inputs_n[_i]);
-    char *expected = memset(buf_orig, memset_correct_inputs_c[_i], memset_correct_inputs_n[_i]);
-
-    // 3. Проверяем результат
-    // Важно: сравнивать нужно не только указатели (они будут разными адресами malloc),
-    // а содержимое памяти! Используем ck_assert_mem_eq, так как в c может быть '\0'
     ck_assert_mem_eq(res, expected, len);
-
-    // 4. Обязательно освобождаем память
-    free(buf_s21);
-    free(buf_orig);
 }
 END_TEST
 
-
-char *memset_edge_inputs_s[] = {"apple", "verter", "alll"};
-int memset_edge_inputs_c[] = {-1, 257, 'l'};
-s21_size_t memset_edge_inputs_n[] = {3, 4, 0};
-
 START_TEST(test_memset_edge_input_orig_compare) {
-    // 1. Создаем два изменяемых буфера в стеке под текущую строку
-    // Выделяем память с запасом (+1 для '\0')
-    s21_size_t len = strlen(memset_edge_inputs_s[_i]) + 1;
-    char *buf_s21 = malloc(len);
-    char *buf_orig = malloc(len);
+    const memsetParams *current_case = get_edge_memset_case(_i);
+    char *res = s21_NULL;
+    char *expected = s21_NULL;
+    s21_size_t len;
+    test_memset_function(*current_case, &res, &expected, &len);
 
-    // Копируем неизменяемый литерал в наши изменяемые буферы
-    strcpy(buf_s21, memset_edge_inputs_s[_i]);
-    strcpy(buf_orig, memset_edge_inputs_s[_i]);
-
-    // 2. Вызываем функции на изменяемой памяти
-    char *res = s21_memset(buf_s21, memset_edge_inputs_c[_i], memset_edge_inputs_n[_i]);
-    char *expected = memset(buf_orig, memset_edge_inputs_c[_i], memset_edge_inputs_n[_i]);
-
-    // 3. Проверяем результат
-    // Важно: сравнивать нужно не только указатели (они будут разными адресами malloc),
-    // а содержимое памяти! Используем ck_assert_mem_eq, так как в c может быть '\0'
     ck_assert_mem_eq(res, expected, len);
-
-    // 4. Обязательно освобождаем память
-    free(buf_s21);
-    free(buf_orig);
 }
 END_TEST
 
@@ -70,11 +77,11 @@ Suite *memset_suite_create(void) {
     Suite *s = suite_create("Memset");
 
     TCase *tc_correct = tcase_create("Correct_Input_Original_Compare");
-    tcase_add_loop_test(tc_correct, test_memset_correct_input_orig_compare, 0, (int)(sizeof(memset_correct_inputs_s) / sizeof(memset_correct_inputs_s[0])));
+    tcase_add_loop_test(tc_correct, test_memset_correct_input_orig_compare, 0, CORRECT_CASES_NUM);
     suite_add_tcase(s, tc_correct);
 
     TCase *tc_edge = tcase_create("Edge_Input_Original_Compare");
-    tcase_add_loop_test(tc_edge, test_memset_edge_input_orig_compare, 0, (int)(sizeof(memset_edge_inputs_s) / sizeof(memset_edge_inputs_s[0])));
+    tcase_add_loop_test(tc_edge, test_memset_edge_input_orig_compare, 0, EDGE_CASES_NUM);
     suite_add_tcase(s, tc_edge);
 
 
