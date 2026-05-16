016f5c82e7c41a202be816425c5b354b8f0879c8
Vanesa Karina <vaneskar@mu-l5.yar.21-school.ru>
2026-05-16 16:20:30 +0300

    [vaneskar:feature/memset-6] Rewrote test

--- src/tests/test_s21_memset.c
+++ src/tests/test_s21_memset.c
@@ -1,12 +1,9 @@
 #include <check.h>
 #include <stdlib.h>
-#include "../s21_string.h"
 #include <string.h>
+#include "../s21_string.h"
 
-#define STRING_CASES_NUM 5
-#define INT_CASES_NUM 3
-#define EDGE_CASES_NUM 3
-
+// Структура параметров теста
 typedef struct {
     const void *data;
     int chr;
@@ -14,116 +11,79 @@ typedef struct {
     s21_size_t data_size;
 } memsetParams;
 
-static const memsetParams* get_string_memset_case(int index) {
-    // Массив изолирован внутри функции и невидим снаружи
-    static const memsetParams cases[] = {
-        {"apple", 'p', 3, 6},
-        {"verter", 'a', 4, 7},
-        {"alll", 'l', 1, 5},
-        {"supercalifragilisticexpialidocious", '\0', 34, 35},
-        {"Several words and 221 122 32", '1', 14, 29}
-    };
-    return &cases[index];
-}
-
-static const memsetParams* get_int_memset_case(int index) {
-    // 1. Создаем отдельные статические массивы для каждого тест-кейса
-    static const int arr1[] = {1, 2, 3, 4, 5};
-    static const int arr2[] = {100, 200, 300};
-    static const int arr3[] = {-1, -2, -3};
-
-    // 2. Инициализируем структуру, передавая указатели на эти массивы
-    static const memsetParams cases[] = {
-        {arr1, '\0', 4 * sizeof(int) + 1, sizeof(arr1)},
-        {arr2, 'b',  3 * sizeof(int), sizeof(arr2)},
-        {arr3, 0,    3 * sizeof(int), sizeof(arr3)}
-    };
-    return &cases[index];
-}
-
-static const memsetParams* get_edge_memset_case(int index) {
-    // Массив изолирован внутри функции и невидим снаружи
-    static const memsetParams cases[] = {
-        {"apple", -1, 3, 6},
-        {"verter", 257, 4, 7},
-        {"alll", 'l', 0, 5}
-    };
-    return &cases[index];
-}
-
+// Функция выделения буфера памяти
 char* create_buffer(s21_size_t len, const void* str) {
     char *buffer = malloc(len);
     ck_assert_ptr_nonnull(buffer);
-
     memcpy(buffer, str, len);
     return buffer;
 }
 
-void test_memset_function(memsetParams current_case, char **res, char **expected, s21_size_t *len) {
-    *len = current_case.data_size;
-    char *buf_s21 = create_buffer(*len, current_case.data);
-    char *buf_orig = create_buffer(*len, current_case.data);
+// Универсальная функция запуска одиночного теста
+void run_memset_test(memsetParams *params) {
+    s21_size_t len = params->data_size;
+    char *buf_s21 = create_buffer(len, params->data);
+    char *buf_orig = create_buffer(len, params->data);
 
-    *res = s21_memset(buf_s21, current_case.chr, current_case.num);
-    *expected = memset(buf_orig, current_case.chr, current_case.num);
-}
+    char *res = s21_memset(buf_s21, params->chr, params->num);
+    char *expected = memset(buf_orig, params->chr, params->num);
 
-START_TEST(test_memset_string_orig_compare) {
-    const memsetParams *current_case = get_string_memset_case(_i);
-    char *res = s21_NULL;
-    char *expected = s21_NULL;
-    s21_size_t len;
-    test_memset_function(*current_case, &res, &expected, &len);
-
+    // Проверяем корректность заполнения всей области памяти буфера
     ck_assert_mem_eq(res, expected, len);
-    free(res);
-    free(expected);
+
+    free(buf_s21);
+    free(buf_orig);
 }
-END_TEST
 
-START_TEST(test_memset_int_orig_compare) {
-    const memsetParams *current_case = get_int_memset_case(_i);
-    char *res = s21_NULL;
-    char *expected = s21_NULL;
-    s21_size_t len;
-    test_memset_function(*current_case, &res, &expected, &len);
+// Макрос для создания тестовых наборов
+#define TEST_CASES(name, ...) \
+    static memsetParams name[] = {__VA_ARGS__}; \
+    START_TEST(test_##name) { \
+        for (size_t i = 0; i < sizeof(name)/sizeof(name[0]); i++) { \
+            run_memset_test(&name[i]); \
+        } \
+    } \
+    END_TEST
 
-    ck_assert_mem_eq(res, expected, len);
-    free(res);
-    free(expected);
-}
-END_TEST
+// 1. Тесты на обычных строках
+TEST_CASES(string_tests,
+    {"apple", 'p', 3, 6},
+    {"verter", 'a', 4, 7},
+    {"alll", 'l', 1, 5},
+    {"supercalifragilisticexpialidocious", '\0', 34, 35},
+    {"Several words and 221 122 32", '1', 14, 29}
+)
 
-START_TEST(test_memset_edge_orig_compare) {
-    const memsetParams *current_case = get_edge_memset_case(_i);
-    char *res = s21_NULL;
-    char *expected = s21_NULL;
-    s21_size_t len;
-    test_memset_function(*current_case, &res, &expected, &len);
+// 2. Тесты на массивах целых чисел (используются составные литералы)
+TEST_CASES(int_tests,
+    {(int[]){1, 2, 3, 4, 5}, '\0', 4 * sizeof(int) + 1, 5 * sizeof(int)},
+    {(int[]){100, 200, 300}, 'b',  3 * sizeof(int),     3 * sizeof(int)},
+    {(int[]){-1, -2, -3},    0,    3 * sizeof(int),     3 * sizeof(int)}
+)
 
-    ck_assert_mem_eq(res, expected, len);
-    free(res);
-    free(expected);
-}
-END_TEST
+// 3. Пограничные и экстремальные случаи
+TEST_CASES(edge_tests,
+    {"apple", -1, 3, 6},
+    {"verter", 257, 4, 7},
+    {"alll", 'l', 0, 5}
+)
 
-
-// Функция, которую вызовет Runner
+// Функция сборки Suite для Runner
 Suite *memset_suite_create(void) {
     Suite *s = suite_create("Memset");
 
-    TCase *tc_correct = tcase_create("String_Original_Compare");
-    tcase_add_loop_test(tc_correct, test_memset_string_orig_compare, 0, STRING_CASES_NUM);
-    suite_add_tcase(s, tc_correct);
-
-    TCase *tc_edge = tcase_create("Edge_Original_Compare");
-    tcase_add_loop_test(tc_edge, test_memset_edge_orig_compare, 0, EDGE_CASES_NUM);
-    suite_add_tcase(s, tc_edge);
+    TCase *tc_string = tcase_create("String_Original_Compare");
+    tcase_add_test(tc_string, test_string_tests);
+    suite_add_tcase(s, tc_string);
 
     TCase *tc_int = tcase_create("Int_Original_Compare");
-    tcase_add_loop_test(tc_int, test_memset_int_orig_compare, 0, INT_CASES_NUM);
+    tcase_add_test(tc_int, test_int_tests);
     suite_add_tcase(s, tc_int);
 
+    TCase *tc_edge = tcase_create("Edge_Original_Compare");
+    tcase_add_test(tc_edge, test_edge_tests);
+    suite_add_tcase(s, tc_edge);
 
     return s;
 }
+
