0407bdf724c31375bf12d3b665dc4fac937dff7c
Vanesa Karina <vaneskar@mu-l5.yar.21-school.ru>
2026-05-18 13:41:01 +0300

    [vaneskar:core_changes-5] Added common header to my tests, added memset suite to test_main

--- src/tests/s21_test_common.h
+++ src/tests/s21_test_common.h
@@ -23,6 +23,25 @@ typedef struct {
     const char *test_name;
 } StrChrTestParams;
 
+// Структура параметров теста
+typedef struct {
+    const void *data;
+    int chr;
+    s21_size_t num;
+    s21_size_t data_size;
+} memsetParams;
+
+typedef struct {
+    const void *str;
+    int c;
+    s21_size_t n;
+} memchrParams;
+
+// Структура параметров теста
+typedef struct {
+    int errnum;
+} strerrorParams;
+
 
 // Макрос для создания тестовых наборов
 #define TEST_CASES(name,param_type ,run_func, ...) \
@@ -39,5 +58,14 @@ typedef struct {
 
 #define STRCHR_TEST_CASES(name, ...) \
     TEST_CASES(name, StrChrTestParams, run_strchr_test, __VA_ARGS__)
+
+#define MEMSET_TEST_CASES(name, ...) \
+    TEST_CASES(name, memsetParams, run_memset_test, __VA_ARGS__)
+
+#define MEMCHR_TEST_CASES(name, ...) \
+    TEST_CASES(name, memchrParams, run_memchr_test, __VA_ARGS__)
+
+#define STRERROR_TEST_CASES(name, ...) \
+    TEST_CASES(name, strerrorParams, run_strerror_test, __VA_ARGS__)
 
 #endif
--- src/tests/test_s21_strerror.c
+++ src/tests/test_s21_strerror.c
@@ -3,11 +3,7 @@
 #include <check.h>
 #include <errno.h>
 #include "../s21_string.h"
-
-// Структура параметров теста
-typedef struct {
-    int errnum;
-} strerrorParams;
+#include "s21_test_common.h"
 
 // Вспомогательная функция проверки (сравниваем строки через стандартную strcmp)
 void run_strerror_test(strerrorParams *params) {
@@ -18,18 +14,8 @@ void run_strerror_test(strerrorParams *params) {
     ck_assert_str_eq(s21_res, orig_res);
 }
 
-// Макрос для автоматического создания цикла проверок
-#define TEST_CASES(name, ...) \
-    static strerrorParams name[] = {__VA_ARGS__}; \
-    START_TEST(test_##name) { \
-        for (size_t i = 0; i < sizeof(name)/sizeof(name[0]); i++) { \
-            run_strerror_test(&name[i]); \
-        } \
-    } \
-    END_TEST
-
 // 1. Стандартные распространенные ошибки
-TEST_CASES(standard_errors,
+STRERROR_TEST_CASES(standard_errors,
     {0},            // No error
     {EPERM},        // Operation not permitted (1)
     {ENOENT},       // No such file or directory (2)
@@ -41,14 +27,14 @@ TEST_CASES(standard_errors,
 )
 
 // 2. Граничные значения кодов операционной системы
-TEST_CASES(edge_errors,
+STRERROR_TEST_CASES(edge_errors,
     {1},            // Минимальная стандартная ошибка
     {106},          // Системная ошибка из конца списка glibc/macOS
     {133}           // Последняя доступная ошибка на некоторых ОС
 )
 
 // 3. Невалидные и критические пограничные случаи (проверка "Unknown error")
-TEST_CASES(invalid_errors,
+STRERROR_TEST_CASES(invalid_errors,
     {-1},           // Отрицательный код ошибки
     {150},          // Несуществующая ошибка (в glibc/macOS их около 133)
     {1000},         // Большое невалидное число
