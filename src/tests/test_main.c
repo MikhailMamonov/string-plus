1f5dddb08e5d0de87d24f08da7fda54248cf9a22
unknown <mamon_2010@mail.ru>
2026-05-18 18:32:22 +0300

    [core_changes-5] - Added fix-style and check-style targets, formatted files.

--- src/tests/test_main.c
+++ src/tests/test_main.c
@@ -1,6 +1,6 @@
+#include "../s21_string.h"
 #include <check.h>
 #include <stdlib.h>
-#include "../s21_string.h"
 
 Suite *memchr_suite_create(void);
 Suite *memcpy_suite_create(void);
@@ -11,22 +11,22 @@ Suite *strcspn_suite_create(void);
 Suite *strrchr_suite_create(void);
 
 void assemble_srunner(SRunner *sr) {
-    srunner_add_suite(sr, memcpy_suite_create());
-    srunner_add_suite(sr, strerror_suite_create());
-    srunner_add_suite(sr, strchr_suite_create());
-    srunner_add_suite(sr, strcspn_suite_create());
-    srunner_add_suite(sr, memset_suite_create());
-    srunner_add_suite(sr, strrchr_suite_create());
+  srunner_add_suite(sr, memcpy_suite_create());
+  srunner_add_suite(sr, strerror_suite_create());
+  srunner_add_suite(sr, strchr_suite_create());
+  srunner_add_suite(sr, strcspn_suite_create());
+  srunner_add_suite(sr, memset_suite_create());
+  srunner_add_suite(sr, strrchr_suite_create());
 }
 
 int main(void) {
-    int failed = 0;
-    SRunner *sr = srunner_create(memchr_suite_create());
-    assemble_srunner(sr);
+  int failed = 0;
+  SRunner *sr = srunner_create(memchr_suite_create());
+  assemble_srunner(sr);
 
-    srunner_run_all(sr, CK_NORMAL);
-    failed = srunner_ntests_failed(sr);
-    srunner_free(sr);
+  srunner_run_all(sr, CK_NORMAL);
+  failed = srunner_ntests_failed(sr);
+  srunner_free(sr);
 
-    return (failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+  return (failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
--- src/tests/test_s21_memchr.c
+++ src/tests/test_s21_memchr.c
@@ -1,61 +1,50 @@
+#include "../s21_string.h"
+#include "s21_test_common.h"
 #include <check.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../s21_string.h"
-#include "s21_test_common.h"
 
 // Универсальная функция запуска одиночного теста
 void run_memchr_test(memchrParams *params) {
-    char *res = s21_memchr(params->str, params->c, params->n);
-    char *expected = memchr(params->str, params->c, params->n);
+  char *res = s21_memchr(params->str, params->c, params->n);
+  char *expected = memchr(params->str, params->c, params->n);
 
-    // Сравниваем точные адреса указателей в памяти
-    ck_assert_ptr_eq(res, expected);
+  // Сравниваем точные адреса указателей в памяти
+  ck_assert_ptr_eq(res, expected);
 }
 
 // 1. Положительные тесты (символ гарантированно есть в области видимости n)
-MEMCHR_TEST_CASES(positive_tests,
-    {"apple", 'p', 3},
-    {"verter", 'r', 6},
-    {"aaa", 'a', 3},
-    {"supercalifragilisticexpialidocious", 'u', 35},
-    {"Several words and 221 122 32", ' ', 14}
-)
+MEMCHR_TEST_CASES(positive_tests, {"apple", 'p', 3}, {"verter", 'r', 6},
+                  {"aaa", 'a', 3},
+                  {"supercalifragilisticexpialidocious", 'u', 35},
+                  {"Several words and 221 122 32", ' ', 14})
 
 // 2. Отрицательные тесты (символ отсутствует или находится за пределами n)
-MEMCHR_TEST_CASES(negative_tests,
-    {"apple", 'p', 1},
-    {"verter", 'a', 6},
-    {"a", 'l', 1},
-    {"supercalifragilisticexpialidocious", 'c', 4},
-    {"Several words and 221 122 32", ' ', 7},
-    {"Spare test", '\0', 5}
-)
+MEMCHR_TEST_CASES(negative_tests, {"apple", 'p', 1}, {"verter", 'a', 6},
+                  {"a", 'l', 1}, {"supercalifragilisticexpialidocious", 'c', 4},
+                  {"Several words and 221 122 32", ' ', 7},
+                  {"Spare test", '\0', 5})
 
 // 3. Пограничные и экстремальные случаи
-MEMCHR_TEST_CASES(edge_tests,
-    {"", '\0', 1},
-    {"", '\n', 0},
-    {"a", '\0', 1},
-    {"supercalifragilisticexpialidocious/", '/', 35},
-    {"Spare test", '\0', 11}
-)
+MEMCHR_TEST_CASES(edge_tests, {"", '\0', 1}, {"", '\n', 0}, {"a", '\0', 1},
+                  {"supercalifragilisticexpialidocious/", '/', 35},
+                  {"Spare test", '\0', 11})
 
 // Функция сборки Suite для Runner
 Suite *memchr_suite_create(void) {
-    Suite *s = suite_create("Memchr");
+  Suite *s = suite_create("Memchr");
 
-    TCase *tc_pos = tcase_create("Positive_Original_Compare");
-    tcase_add_test(tc_pos, test_positive_tests);
-    suite_add_tcase(s, tc_pos);
+  TCase *tc_pos = tcase_create("Positive_Original_Compare");
+  tcase_add_test(tc_pos, test_positive_tests);
+  suite_add_tcase(s, tc_pos);
 
-    TCase *tc_neg = tcase_create("Negative_Original_Compare");
-    tcase_add_test(tc_neg, test_negative_tests);
-    suite_add_tcase(s, tc_neg);
+  TCase *tc_neg = tcase_create("Negative_Original_Compare");
+  tcase_add_test(tc_neg, test_negative_tests);
+  suite_add_tcase(s, tc_neg);
 
-    TCase *tc_edge = tcase_create("Edge_Original_Compare");
-    tcase_add_test(tc_edge, test_edge_tests);
-    suite_add_tcase(s, tc_edge);
+  TCase *tc_edge = tcase_create("Edge_Original_Compare");
+  tcase_add_test(tc_edge, test_edge_tests);
+  suite_add_tcase(s, tc_edge);
 
-    return s;
+  return s;
 }
