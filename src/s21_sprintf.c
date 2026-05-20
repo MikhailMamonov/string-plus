3722ffe966f1b84c0a73bdf01ca273fe84b1c7e8
Britt Dennis <brittden@mu-j3.yar.21-school.ru>
2026-05-20 14:28:16 +0300

    [feature/sprintf-4] - implemented filling formatSpec

--- src/s21_sprintf.c
+++ src/s21_sprintf.c
@@ -3,11 +3,15 @@
 
 
 typedef struct {
-    int flags;
-    int width;
-    int precision;
-    char length;
-    char specifier;
+    unsigned int left_align : 0; // флаг '-'
+    unsigned int show_sign : 0;// флаг '+'
+    unsigned int space_sign : 1; // флаг ' ' 
+    unsigned int alt_format : 1;  // флаг '#'
+    unsigned int zero_padding : 1;   // флаг '0'
+    int width; // ширина '-'
+    int precision; // точность
+    char length;// длина
+    char specifier; // спецификатор
 } formatSpec;
 
 
@@ -15,27 +19,96 @@ int parseFlag(char input){
     return (input == '+' || input == '-' || input == ' ' || input == '#' || input == '0');
 }
 
-int parseWidth(char input){
-     return (input == '*' || isdigit(input));
-}
-
-void parseAccuracy(char **input){
-    if(**input == '.' ){
-        (*input)++;
-        while(isdigit(**input))
-        {
-            (*input)++;
+char* parseWidth(char *input, formatSpec * spec, va_list* args){
+     if (*input == '*') {
+        spec->width = va_arg(*args, int);
+        input++;
+    } else if (isdigit(*input)) {
+        spec->width = 0;
+        while (isdigit(*input)) {
+            spec->width = spec->width * 10 + (*input - '0');
+            input++;
         }
     }
+    return input;
 }
 
-int parseLength(char input){
-    return (input == 'l' || input == 'L' || input == 'h');
+char* parsePrecision(char *input, formatSpec * spec, va_list* args){
+    if(*input == '.'){
+        input++;
+        if (*input == '*') {
+        spec->precision = va_arg(*args, int);
+        input++;
+    } else if (isdigit(*input)) {
+        spec->precision = 0;
+        while (isdigit(*input)) {
+            spec->precision = spec->precision * 10 + (*input - '0');
+            input++;
+        }
+    }
+    else{
+        spec->precision = 0;
+    }
+    } 
+    return input;
+}
+
+
+char* parseLength(char *format, formatSpec * spec){
+    if(input == 'l' || input == 'L' || input == 'h'){
+        spec->length = *input;
+        input++;
+        if (spec->length == 'l' && *input == 'l') {
+            spec->length = 'L';  // или введите отдельный флаг
+            input++;
+        }
+        // Поддержка short short (hh)
+        if (spec->length == 'h' && *input == 'h') {
+            spec->length = 'H';  // или введите отдельный флаг
+            input++;
+        }
+    }
+
+    return input;
 }
 
 int parseSpecifier(char input){
     return (input && s21_strchr("diouxXeEfgGcspn%", input) != NULL);
 }
+
+char* parseFormat(char *format, formatSpec * spec, va_list* args){
+    memset(spec, 0, sizeof(formatSpec));
+    spec->width = -1;
+    spec->precision = -1;
+    //Parse flags
+    while(parseFlag(*format))
+    {
+        switch (*format) {
+            case '-': spec->left_align = 1; break;
+            case '+': spec->show_sign = 1; break;
+            case ' ': spec->space_sign = 1; break;
+            case '#': spec->alt_format = 1; break;
+            case '0': spec->zero_padding = 1; break;
+        }
+        format++;
+    } 
+    // Parse width
+    format = parseWidth(format, spec, args);
+
+    //Parse precision
+    format = parsePrecision(format, spec, args);
+
+    //Parse length
+    format = parseLength(format, spec);
+
+     if (parseSpecifier(*format)) {
+        spec->specifier = *format;
+        format++;
+    }
+
+
+
+} 
 
 int s21_sprintf(char *str, const char *format, ...) {
     int len = 0;
@@ -48,20 +121,9 @@ int s21_sprintf(char *str, const char *format, ...) {
                 len++;  
                 continue;
             }
-            //Parse flags
-            while(parseFlag(*format)){
-                format++;
-            } 
-            // Parse width
-            while(parseWidth(*format)){
-                format++;
-            }
-            //Parse accuracy
-            parseAccuracy(&format);
+            formatSpec spec = {0};
+            format = parseFormat(format, &spec); 
 
-            if(parseLength(*format)){
-                format++;
-            }
 
             //Parse specifier
             if(parseSpecifier(*format)){
