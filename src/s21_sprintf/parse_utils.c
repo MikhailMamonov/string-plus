4adbd3794d7d8e1e4018a34e7e9edd1174f845cc
Britt Dennis <brittden@mu-j3.yar.21-school.ru>
2026-05-20 16:28:47 +0300

    [feature/sprintf-6] - refactored main function,produced decomposition, changed parseSpecifier

---
+++ src/s21_sprintf/parse_utils.c
@@ -0,0 +1,140 @@
+#include "sprintf_utils.h"
+#include <string.h>  // для memset
+#include <ctype.h> 
+
+int parseFlag(char input){
+    return (input == '+' || input == '-' || input == ' ' || input == '#' || input == '0');
+}
+
+char* parseWidth(char *input, formatSpec * spec, va_list* args){
+     if (*input == '*') {
+        spec->width = va_arg(*args, int);
+        if (spec->width < 0) {
+            spec->left_align = 1;
+            spec->width = -spec->width;
+        }
+        input++;
+    } else if (isdigit(*input)) {
+        spec->width = 0;
+        while (isdigit(*input)) {
+            spec->width = spec->width * 10 + (*input - '0');
+            input++;
+        }
+    }
+    
+    return input;
+}
+
+char* parsePrecision(char *input, formatSpec * spec, va_list* args){
+    if(*input == '.'){
+        input++;
+        if (*input == '*') {
+        spec->precision = va_arg(*args, int);
+        if (spec->precision < 0) {
+                spec->precision = -1;  // Отрицательная точность игнорируется
+        }
+        input++;
+    } else if (isdigit(*input)) {
+        spec->precision = 0;
+        while (isdigit(*input)) {
+            spec->precision = spec->precision * 10 + (*input - '0');
+            input++;
+        }
+    }
+    } 
+    return input;
+}
+
+
+char* parseLength(char *input, formatSpec * spec){
+    if(*input == 'l' || *input == 'L' || *input == 'h'){
+        spec->length = *input;
+        input++;
+        if (spec->length == 'l' && *input == 'l') {
+            spec->length = 'L';  // или введите отдельный флаг
+            input++;
+        }
+        // Поддержка short short (hh)
+        if (spec->length == 'h' && *input == 'h') {
+            spec->length = 'H';  // или введите отдельный флаг
+            input++;
+        }
+    }
+
+    return input;
+}
+
+int parseSpecifier(char input, formatSpec *spec) {
+    if (input && s21_strchr("diouxXeEfgGcspn%", input) != NULL) {
+        spec->specifier = input;
+        return 1; 
+    }
+    return 0;
+}
+
+// Отдельная функция для форматирования
+void formatBySpecifier(formatSpec *spec, va_list *args, char **out) {
+    switch (spec->specifier) {
+        case 'c': {
+            char c = (char)va_arg(*args, int);
+            *(*out)++ = c;
+            break;
+        }
+        case 's': {
+            char *s = va_arg(*args, char*);
+            while (*s) {
+                *(*out)++ = *s++;
+            }
+            break;
+        }
+        case 'd': {
+            int d = va_arg(*args, int);
+            *out = int_to_str(*out, d);
+            break;
+        }
+        case '%': {
+            *(*out)++ = '%';
+            break;
+        }
+        default: {
+            *(*out)++ = '%';
+            if (spec->specifier) {
+                *(*out)++ = spec->specifier;
+            }
+            break;
+        }
+    }
+}
+
+char* parseFormat(char *format, formatSpec * spec, va_list* args, char **out){
+    memset(spec, 0, sizeof(formatSpec));
+    spec->width = -1;
+    spec->precision = -1;
+    //Parse flags
+    while(parseFlag(*format))
+    {
+        switch (*format) {
+            case '-': spec->left_align = 1; break;
+            case '+': spec->show_sign = 1; break;
+            case ' ': spec->space_sign = 1; break;
+            case '#': spec->alt_format = 1; break;
+            case '0': spec->zero_padding = 1; break;
+        }
+        format++;
+    } 
+    // Parse width
+    format = parseWidth(format, spec, args);
+
+    //Parse precision
+    format = parsePrecision(format, spec, args);
+
+    //Parse length
+    format = parseLength(format, spec);
+
+    if (parseSpecifier(*format, spec)) {
+        format++; 
+        formatBySpecifier(spec, args, out);
+    }
+
+    return format;
+}
--- src/s21_sprintf/sprintf_utils.h
+++ src/s21_sprintf/sprintf_utils.h
@@ -1,7 +1,27 @@
 #ifndef SPRINTF_UTILS_H
 #define SPRINTF_UTILS_H
 
+typedef struct {
+    unsigned int left_align : 1; // флаг '-'
+    unsigned int show_sign : 1;// флаг '+'
+    unsigned int space_sign : 1; // флаг ' ' 
+    unsigned int alt_format : 1;  // флаг '#'
+    unsigned int zero_padding : 1;   // флаг '0'
+    int width; // ширина '-'
+    int precision; // точность
+    char length;// длина
+    char specifier; // спецификатор
+} formatSpec;
+
+
 char* int_to_str(char *buf, int num);
+int parseFlag(char input);
+char* parseWidth(char *input, formatSpec * spec, va_list* args);
+char* parsePrecision(char *input, formatSpec * spec, va_list* args);
+char* parseLength(char *format, formatSpec * spec);
+int parseSpecifier(char input, formatSpec *spec); 
+char* parseFormat(char *format, formatSpec * spec, va_list* args,  char **out);
+void formatBySpecifier(formatSpec *spec, va_list *args, char **out);
 
 
 #endif
