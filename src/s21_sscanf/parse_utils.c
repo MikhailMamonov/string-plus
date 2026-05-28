4e10839d10629f14eddbb80703edd104d6659e30
Britt Dennis <brittden@mu-j3.yar.21-school.ru>
2026-05-28 15:07:31 +0300

    [brittden:feature/sscanf-2] parsing format string implemented..

--- src/s21_sscanf/parse_utils.c
+++ src/s21_sscanf/parse_utils.c
@@ -1,75 +1,116 @@
 #include "s21_sscanf.h"
 #include <ctype.h>
 
-int parseFlag(char input) {
-    static const char *flags = "+- #0";
-    return (s21_strchr(flags, input) != s21_NULL);
-}
+const char *parseScanSet(const char *input, formatSpec *spec)
+{
+  input++;
 
-const char* parseFlags(const char *format, formatSpec *spec) {
-    while (*format && parseFlag(*format)) {
-        switch (*format) {
-            case '-': spec->left_align = 1; break;
-            case '+': spec->show_sign = 1; break;
-            case ' ': spec->space_sign = 1; break;
-            case '#': spec->alt_format = 1; break;
-            case '0': spec->zero_padding = 1; break;
-        }
-        format++;
-    }
-    return format;
-}
-
-const char *parseWidth(const char *input, formatSpec *spec, va_list *args) {
-  if (*input == '*') {
-    spec->width = va_arg(*args, int);
-    if (spec->width < 0) {
-      spec->left_align = 1;
-      spec->width = -spec->width;
-    }
+  // Обработка инвертированного набора %[^...]
+  if (*input == '^')
+  {
+    spec->inverted = 1;
     input++;
-  } else if (isdigit(*input)) {
-    spec->width = 0;
-    while (isdigit(*input)) {
-      spec->width = spec->width * 10 + (*input - '0');
+  }
+
+  // Чтение паттерна до ']'
+  s21_size_t pos = 0;
+
+  if (*input == ']')
+  {
+    if (pos < MAX_PATTERN_LEN - 1)
+    {
+      spec->scan_set[pos++] = ']';
       input++;
     }
+  }
+
+  while (*input && *input != ']' && pos < MAX_PATTERN_LEN - 1)
+  {
+    spec->scan_set[pos++] = *input;
+    input++;
+  }
+
+  if (*input == ']')
+  {
+    spec->scan_set[pos] = '\0';
+    input++;
+  }
+  else
+  {
+    // Ошибка: нет закрывающей ']'
+    spec->scan_set[0] = '\0';
+    return s21_NULL;
   }
 
   return input;
 }
 
-const char *parsePrecision(const char *input, formatSpec *spec, va_list *args) {
-  if (*input == '.') {
-    input++;
-    if (*input == '*') {
-      spec->precision = va_arg(*args, int);
-      if (spec->precision < 0) {
-            spec->precision = -1;
-        }
+const char *parseWidth(const char *input, formatSpec *spec)
+{
+  // Сброс состояния
+  spec->use_suppress = 0;
+  spec->width = 0;
+  spec->length = 0;
+  spec->scan_set[0] = '\0';
+  spec->inverted = 0;
+
+  if (isdigit(*input))
+  {
+    spec->width = 0;
+    while (isdigit(*input))
+    {
+      spec->width = spec->width * 10 + (*input - '0');
       input++;
-    } else if (isdigit(*input)) {
-      spec->precision = 0;
-      while (isdigit(*input)) {
-        spec->precision = spec->precision * 10 + (*input - '0');
+    }
+  }
+
+  if (*input == '*')
+  {
+    spec->use_suppress = 1;
+    input++;
+    if (isdigit(*input))
+    {
+      // Не обнуляем! width уже может быть установле
+      unsigned int w = 0;
+      while (isdigit(*input))
+      {
+        w = w * 10 + (*input - '0');
         input++;
       }
+      if (!spec->width)
+      {
+        spec->width = w;
+      }
     }
-  } else
-    spec->precision = -1;
+  }
+
+  if (*input == '[')
+  {
+    const char *next = parseScanSet(input, spec);
+    if (!next)
+    {                  // Проверка на ошибку
+      return s21_NULL; // или input, в зависимости от дизайна
+    }
+    input = next;
+  }
+
   return input;
 }
 
-const char *parseLength(const char *input, formatSpec *spec) {
-  if (*input == 'l' || *input == 'L' || *input == 'h') {
+const char *parseLength(const char *input, formatSpec *spec)
+{
+  if (*input == 'l' || *input == 'L' || *input == 'h')
+  {
     spec->length = *input;
     input++;
-    if (spec->length == 'l' && *input == 'l') {
+    if (spec->length == 'l' && *input == 'l')
+    {
       spec->length = 'L'; // или введите отдельный флаг
       input++;
     }
     // Поддержка short short (hh)
-    if (spec->length == 'h' && *input == 'h') {
+    if (spec->length == 'h' && *input == 'h')
+    {
       spec->length = 'H'; // или введите отдельный флаг
       input++;
     }
@@ -78,25 +119,37 @@ const char *parseLength(const char *input, formatSpec *spec) {
   return input;
 }
 
-const char* parseSpecifier(const char *format, formatSpec *spec) {
-   static const char *specifiers = "diouxXeEfgGcspn%";
-    
-    if (format && s21_strchr(specifiers, *format)) {
-        spec->specifier = *format;
-        return format + 1;
-    }
-    return format;
+const char *parseSpecifier(const char *format, formatSpec *spec)
+{
+  static const char *specifiers = "diouxXeEfgGcspn%";
+
+  if (format && s21_strchr(specifiers, *format))
+  {
+    spec->specifier = *format;
+    return format + 1;
+  }
+  return format;
 }
 
-const char *parseFormat(const char *format, formatSpec *spec, va_list *args) {
+const char *parseFormat(const char *format, formatSpec *spec)
+{
   s21_memset(spec, 0, sizeof(formatSpec));
-  spec->precision = -1;  
 
-  format = parseFlags(format, spec);
-  format = parseWidth(format, spec, args);
-  format = parsePrecision(format, spec, args);
+  format = parseWidth(format, spec);
+  if (!format)
+  {
+    return s21_NULL;
+  }
   format = parseLength(format, spec);
+
+  if (!format)
+  {
+    return s21_NULL;
+  }
   format = parseSpecifier(format, spec);
-    
+  if (!format)
+  {
+    return s21_NULL;
+  }
   return format;
 }
