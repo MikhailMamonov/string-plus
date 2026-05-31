b35fe5a705fc2ad1e0a9b870a4821f00fbd8b5e4
Ваше Имя <you@example.com>
2026-05-31 20:39:11 +0600

    [venaerne: feature/insert - 2] - edited s21_insert.c with Pointer arithmetic

--- src/s21_insert.c
+++ src/s21_insert.c
@@ -1,25 +1,39 @@
-#include <stdlib.h>
 #include "s21_string.h"
+#include <stdlib.h>
 
 void *s21_insert(const char *src, const char *str, s21_size_t start_index) {
-    if (src == s21_NULL || str == s21_NULL) return s21_NULL;
+  if (src == s21_NULL || str == s21_NULL) {
+    return s21_NULL;
+  }
 
-    s21_size_t src_len = s21_strlen(src);
-    if (start_index > src_len) return s21_NULL;
+  s21_size_t src_len = s21_strlen(src);
+  if (start_index > src_len) {
+    return s21_NULL;
+  }
 
-    s21_size_t str_len = s21_strlen(str);
-    char *result = malloc(src_len + str_len + 1);
-    if (result == s21_NULL) return s21_NULL;
+  s21_size_t str_len = s21_strlen(str);
+  char *result = malloc(src_len + str_len + 1);
+  if (result == s21_NULL) {
+    return s21_NULL;
+  }
 
-    for (s21_size_t i = 0; i < start_index; i++)
-        result[i] = src[i];
+  char *p_res = result;
+  const char *p_src = src;
+  const char *p_str = str;
 
-    for (s21_size_t i = 0; i < str_len; i++)
-        result[start_index + i] = str[i];
+  for (s21_size_t i = 0; i < start_index; i++) {
+    *p_res++ = *p_src++;
+  }
 
-    for (s21_size_t i = start_index; i < src_len; i++)
-        result[str_len + i] = src[i];
+  while (*p_str != '\0') {
+    *p_res++ = *p_str++;
+  }
 
-    result[src_len + str_len] = '\0';
-    return result;
+  while (*p_src != '\0') {
+    *p_res++ = *p_src++;
+  }
+
+  *p_res = '\0';
+
+  return result;
 }
