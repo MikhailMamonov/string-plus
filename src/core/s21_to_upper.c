cfe19066fd8b11ff7aba80cef6c7aeda4f0cc6e0
venaerne <chervibl@yandex.ru>
2026-06-02 22:23:54 +0600

    [venaerne: feature/to_upper - 2] turned magical number to var

--- src/core/s21_to_upper.c
+++ src/core/s21_to_upper.c
@@ -1,21 +1,21 @@
-#include "s21_string.h"
+#include "../s21_string.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 void *s21_to_upper(const char *str){
     if(str == s21_NULL) return s21_NULL;
 
     s21_size_t len = s21_strlen(str);
     char *result = malloc(len + 1);
+    char *p_res = result;
     if (result == s21_NULL) return s21_NULL;
     s21_size_t CASE = 'a' - 'A';
     for(s21_size_t i = 0; i < len; i++)
     {
-        if(str[i] >= 'a' && str[i] <= 'z')
-            result[i] = str[i] - CASE;
-        else result[i] = str[i];
+        if(*str >= 'a' && *str <= 'z')
+            *p_res++ = *str++ - CASE;
+        else *p_res++ = *str++;
     }
-    result[len] = '\0';
+    *p_res = '\0';
 
     return result;
 }
